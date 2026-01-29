aceaf4293ffecdc0e61d2b8c669d90f4bd52b17b
Pranav <mohnapranav@gmail.com>
2026-01-29 21:12:34 +0530

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,8 +1,11 @@
-a=int(input("enter your score percentage"))
-if(a>70):
-     print("you are elgible")
-     name=str(input("enter your name"))
-     dep=str(input("enter you dep"))
-     print("succesfull")
+a=int(input("Enter your tamil mark:"))
+b=int(input("Enter your english mark:"))
+c=int(input("Enter your maths mark:"))
+d=int(input("Enter your physics mark:"))
+e=int(input("Enter your cemestry mark:"))
+f=(a+b+c+d+e)/5
+
+if(f<35):
+     print("Additional class is required")
 else:
-     print("you are not elgible")
+     print("keep it-up")
