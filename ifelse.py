5ccf16e6bdbdc586bec0bbbd8a3f6ed0c8e5a72d
Pranav <mohnapranav@gmail.com>
2026-01-29 19:46:01 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+a=int(input("enter your score percentage"))
+if(a>70):
+     print("you are elgible")
+     name=str(input("enter your name"))
+     dep=str(input("enter you dep"))
+     print("succesfull")
+else:
+     print("you are not elgible")
