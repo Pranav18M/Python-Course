69f6f161fbbc1178451457b5856ab387421f2d27
Pranav <mohnapranav@gmail.com>
2026-01-31 11:37:47 +0530

    submit
    Refactor code for counting multiples of 3 and 5, and calculate average of user input.

--- for loop.py
+++ for loop.py
@@ -3,13 +3,26 @@ for i in range(1,11):
     print(i)
 
 
-
-
-
+# qus num 2
 
 count=0
 for i in range(1,101):
     if(i%3==0 and i%5==0):
         count=count+1
 print(count)        
-        
+
+
+# qus num 3
+
+a=[]
+for i in range(5):
+    b=int(input())
+    a.append(b)
+print(a)    
+
+sum=0
+for i in a:
+    sum=sum+i
+    sum=sum/5
+print(sum)    
+
